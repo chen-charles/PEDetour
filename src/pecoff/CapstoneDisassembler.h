ca93ed33803f325a0d7cc920fca81bf35c10c510
chen-charles <chen-charles@users.noreply.github.com>
2019-01-13 16:53:27 -0500

    up-ver capstone

--- src/pecoff/CapstoneDisassembler.h
+++ src/pecoff/CapstoneDisassembler.h
@@ -16,7 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <string>
-#include <capstone.h>
+#include <capstone/capstone.h>
 
 class CapstoneDisassembler
 {
